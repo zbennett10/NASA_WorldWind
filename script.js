ac1fb62df9baf8959d05f2e46970206738b21850
Zachary Bennett <zbennett10@hotmail.com>
2017-01-13 21:43:50 -0500

    successfully placed placemark layer with placemark on map

--- script.js
+++ script.js
@@ -26,14 +26,35 @@ function createLayers() {
 
     //adds basic image layers to world wind
     worldWindWindow.addLayer(new WorldWind.BMNGOneImageLayer());
+    worldWindWindow.addLayer(new WorldWind.BMNGLandsatLayer());
     worldWindWindow.addLayer(new WorldWind.BingAerialWithLabelsLayer());
 
      //add compass layer
     worldWindWindow.addLayer(new WorldWind.CompassLayer());
     worldWindWindow.addLayer(new WorldWind.CoordinatesDisplayLayer(worldWindWindow));
     worldWindWindow.addLayer(new WorldWind.ViewControlsLayer(worldWindWindow));
+
+    //practice putting a pin on map
+    //instantiate placemark object and set properties
+    var placemark = new WorldWind.Placemark(new WorldWind.Position(47.684444, -121,129722, 1e2), true, null);
+        placemark.label = "Practice";
+        placemark.altitudeMode = WorldWind.RELATIVE_TO_GROUND;
+
+    //instantiate attribute object for placemark and set properties
+    var placemarkAttributes = new WorldWind.PlacemarkAttributes(null);
+        placemarkAttributes.drawLeaderLine = true;
+        placemarkAttributes.leaderLineAttributes.outlineColor = WorldWind.Color.RED;
+
+    //instantiate placemark layer
+    var placemarkLayer = new WorldWind.RenderableLayer("Placemarks");
+
+    //apply placemark attributes to placemark
+    placemark.attributes = placemarkAttributes;
+    placemarkLayer.addRenderable(placemark); //apply placemark to layer
+    worldWindWindow.addLayer(placemarkLayer); //apply layer to map
 }
 
+//xml request to NASA API for Ground Stations
 function requestGroundStations() {
     var xmlRequest = new XMLHttpRequest();
     xmlRequest.onreadystatechange = function() {
@@ -45,12 +66,17 @@ function requestGroundStations() {
     xmlRequest.send();
 }
 
+//parses xml data 
 function parseGroundStations(xml) {
     const xmlDoc = xml.responseXML;
     const stationNames =  $.makeArray(xmlDoc.getElementsByTagName("Name")).map(node => node.innerHTML);
     const stationLongitudes = $.makeArray(xmlDoc.getElementsByTagName("Longitude")).map(node => node.innerHTML);
     const stationLatitudes = $.makeArray(xmlDoc.getElementsByTagName("Latitude")).map(node => node.innerHTML);
     
+
     
-    console.log(stationNames, longitudes, latitudes);
+    //console.log(stationNames, stationLongitudes, stationLatitudes);
 }
+
+//create placemark layer - put in load function or new function
+//write function that takes station array data and places a pin on the layer for each station
